7a6ba43ca5b0d71c76400cbeef3392c58d0beb5d
Your Name <you@example.com>
2024-01-23 17:04:10 +0100

    ,

---
+++ database/migrations/2024_01_23_151515_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->tinyText("Azienda");
+            $table->tinyText("Stazione di partenza");
+            $table->tinyText("Stazione di arrivo");
+            $table->time("Orari partenza");
+            $table->time("Orari arrivo");
+            $table->char("Codice treno", 25)->unique();
+            $table->smallInteger("Numero carrozze")->unsigned();
+            $table->boolean("In orario");
+            $table->boolean("Cancellato");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
