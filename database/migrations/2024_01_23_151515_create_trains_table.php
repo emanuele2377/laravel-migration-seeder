a205ae00697d6565a925ed6274371e0f33b22e40
Your Name <you@example.com>
2024-01-23 17:27:07 +0100

    prova 3

--- database/migrations/2024_01_23_151515_create_trains_table.php
+++ database/migrations/2024_01_23_151515_create_trains_table.php
@@ -23,9 +23,6 @@ return new class extends Migration
             $table->boolean("In orario");
             $table->boolean("Cancellato");
             $table->timestamps();
-
-
-            bghfdghjk
         });
     }
 
